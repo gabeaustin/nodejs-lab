001ab9f38d119dbbdf8eca72826b1958f0122eeb
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-03-04 17:08:49 -0600

    added code to push info to reddit array

--- server/server.js
+++ server/server.js
@@ -1,6 +1,24 @@
 const path = require("path");
 const fs = require("fs");
-const requestPromise = require("request-promise");
+const request = require("request-promise");
+const redditArr = [];
+
+request("https://reddit.com/r/popular.json", function (error, response, body) {
+    if(error) {
+        console.log(error);
+    }
+
+    JSON.parse(body).data.children.forEach(item => {
+        let obj = {
+            title: item.data.title,
+            url: item.data.url,
+            author: item.data.author
+        };
+        redditArr.push(obj);
+    });
+    fs.writeFileSync("./popular-articles.json", JSON.stringify(redditArr), (err) => console.log(err)); 
+});
+
 
 let chirpArr = [
     {
@@ -32,4 +50,5 @@ let chirpArr = [
 
 fs.writeFileSync("./chirps.json", JSON.stringify(chirpArr), (err) => console.log(err)); 
 // (file to read, character in coding(usually utf-8))
+
 
