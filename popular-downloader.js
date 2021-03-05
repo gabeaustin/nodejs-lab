35aa1110e8871079b193df356aac73eb1642849f
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-03-05 11:45:25 -0600

    put path.extname in a variable

--- popular-downloader.js
+++ popular-downloader.js
@@ -2,37 +2,29 @@ const path = require("path");
 const fs = require("fs");
 const request = require("request-promise");
 const extensionArr = [];
+const extName = path.extname;
 
 request("https://reddit.com/r/popular.json", function (error, response, body) {
-    if(error) {
+    if (error) {
         console.log(error);
     }
 
-    JSON.parse(body).data.children.forEach(item => {
+    JSON.parse(body).data.children.forEach(article => {
         let extObj = {
-            title: item.data.title,
-            thumbnail: item.data.thumbnail
+            id: article.data.id,
+            title: article.data.title,
+            url: article.data.url
         }
 
-        // path.extname
-
-        // if(extObj.thumbnail === ".jpg" || extObj.thumbnail === ".png" || extObj.thumbnail === ".gif") {
-           if(path.extname(extObj.thumbnail) === ".png" || path.extname(extObj.thumbnail) === ".jpg" || path.extname(extObj.thumbnail) === ".gif") {
-        // if((path.c() === ".jpg") || 
-            // (path.extname() === ".gif") || 
-        //     (path.extname() === ".png")) {
-        // if((path.extname() === ".jpg") || 
-        //     (path.extname() === ".gif") || 
-        //     (path.extname() === ".png")) {
-        //     console.log(item.data.thumbnail)
+        if (extName(article.data.url) == ".png" || 
+            extName(article.data.url) == ".jpg" || 
+            extName(article.data.url) == ".gif" ||
+            extName(article.data.url) == ".gifv") {
             extensionArr.push(extObj);
-           
-        // } else {
-        //     extensionArr.push(item.data.thumbnail);
         }
     });
-    
-    fs.writeFileSync("./downloads/popular-downloader.json", JSON.stringify(extensionArr), (err) => console.log(err)); 
+
+    fs.writeFileSync("./downloads/popular-downloader.json", JSON.stringify(extensionArr), (err) => console.log(err));
 });
 
 
