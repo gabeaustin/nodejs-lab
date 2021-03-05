a42bba76a4a82730b9e0aa867fddbc204be2a45d
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-03-04 20:35:31 -0600

    moved reddit code out to server.js file

--- server/server.js
+++ server/server.js
@@ -1,24 +1,6 @@
 const path = require("path");
 const fs = require("fs");
 const request = require("request-promise");
-const redditArr = [];
-
-request("https://reddit.com/r/popular.json", function (error, response, body) {
-    if(error) {
-        console.log(error);
-    }
-
-    JSON.parse(body).data.children.forEach(item => {
-        let obj = {
-            title: item.data.title,
-            url: item.data.url,
-            author: item.data.author
-        };
-        redditArr.push(obj);
-    });
-    fs.writeFileSync("./popular-articles.json", JSON.stringify(redditArr), (err) => console.log(err)); 
-});
-
 
 let chirpArr = [
     {
